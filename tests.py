95f22efc1137c0e182efc84c51441137e70136ee
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2014-07-02 17:33:58 +0300

    Uh-oh, break my test

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        answer = 42
+        answer = 41
         self.assertEqual(answer, 42)
 
 unittest.main()
